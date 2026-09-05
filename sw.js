815cd4880e7eb8b0c7e72f60ab584206f3326efe
sajomtech-commits <sajomtech-commits@users.noreply.github.com>
2026-09-05 12:20:52 +0200

    refactor: page épurée - 3 boutons simples + app géoloc interactive - analyse complète 12/17/530

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bus-lancon-v1';
-const ASSETS=['./','./index.html','./manifest.json','./pdfs/ligne-12.pdf','./pdfs/ligne-17.pdf','./pdfs/ligne-580.pdf'];
+const CACHE='bus-lancon-v2';
+const ASSETS=['./','./index.html','./manifest.json','./horaires.json','./pdfs/ligne-12.pdf','./pdfs/ligne-12-retour.pdf','./pdfs/ligne-17.pdf','./pdfs/ligne-530.pdf'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{caches.open(CACHE).then(c=>c.put(e.request,res.clone()));return res})))});
